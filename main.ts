ee4db41585ddc0c579dff68edede3a1db13e0d2b
Filipe <fm2097ad@gmail.com>
2015-07-28 14:39:08 +0100

    code cleaning

--- main.ts
+++ main.ts
@@ -17,14 +17,101 @@ const enum VK {
     DOWN = 40, // down arrow
     SPACE = 32 // space bar
 };
-    
+
+const OUT = 'out';
+const DIR = 32; // number of directions for ship
+
+class Ship {
+
+    public body: any;
+
+    constructor(x: number, y: number) {
+        this.body = Matter.Bodies.fromVertices(
+            x, y, // initial position
+            Matter.Vertices.create(Ship.vertices),
+            {
+                // friction: 0.01
+                restitution: 0.5
+            });
+    }
+
+    brake(){
+        Matter.Body.setVelocity(this.body, {
+            x: this.body.velocity.x * Ship.factor,
+            y: this.body.velocity.y * Ship.factor
+        });
+        Matter.Body.setAngularVelocity(this.body, this.body.angularVelocity * Ship.factor);
+    }
+
+    thrust(){
+        const forceMagnitude = 0.0005 * this.body.mass;
+        Matter.Body.applyForce(this.body, this.body.position,
+            Matter.Vector.rotate({ x: 0, y: -forceMagnitude }, this.body.angle)
+            );
+    }
+
+    turnLeft(){
+        Matter.Body.rotate(this.body, -Math.PI / DIR);
+        Matter.Body.setAngularVelocity(this.body, 0);
+    }
+
+    turnRight(){
+        Matter.Body.rotate(this.body, Math.PI / DIR);
+        Matter.Body.setAngularVelocity(this.body, 0);
+    }
+
+    rotate(isLeft : boolean){
+        const m = isLeft ? 1 : -1;
+        //var forceMagnitude = 25 * body.mass;
+        const forceMagnitude = 0.001 * this.body.mass;
+        const mx = 5; // was 50, 200
+        const my = 20;
+
+        Matter.Body.applyForce(this.body, Matter.Vector.rotate({ x: m * (-mx), y: m * my }, this.body.angle),
+            Matter.Vector.rotate({ x: 0, y: forceMagnitude }, this.body.angle)
+        );
+
+        Matter.Body.applyForce(this.body, Matter.Vector.rotate({ x: m * mx, y: m * (-my) }, this.body.angle),
+            Matter.Vector.rotate({ x: 0, y: -forceMagnitude }, this.body.angle)
+        );
+    }
+
+    // these vertices build the ship's shape.
+    static vertices = [{ x: 0, y: 0 }, { x: 10, y: 0 }, { x: 5, y: -20 }];
+    static factor = 0.99;
+};
+
+module Bounds {
+
+    export enum Position {
+        UP, DOWN, LEFT, RIGHT
+    };
+
+    const PADDING = 10;
+    const WIDTH = PADDING * 2; // 'position' is center
+
+    export function make(w: number, h : number, p : Position){
+        switch(p){
+            case Position.UP:
+                return Matter.Bodies.rectangle(w / 2, 0, w, WIDTH, { isStatic: true })
+            case Position.DOWN:
+                return Matter.Bodies.rectangle(w / 2, h, w, WIDTH, { isStatic: true });
+            case Position.LEFT:
+                return Matter.Bodies.rectangle(0, h / 2, WIDTH, h, { isStatic: true });
+            case Position.RIGHT:
+                return Matter.Bodies.rectangle(w, h / 2, WIDTH, h, { isStatic: true });
+        }
+    };
+
+};
+
 // entry function
 function main() {
 
+    // requires at least 800*600 window
     const W = Math.max(800,window.innerWidth);
     const H = Math.max(600, window.innerHeight);
-
-    const out = document.getElementById('out');
+    const hud = document.getElementById(OUT);
 
     // Matter.js module aliases
     const Engine = Matter.Engine,
@@ -36,55 +123,36 @@ function main() {
 
     // create a Matter.js engine and render
     const engine = Engine.create({
+        timing: {
+            timeScale: 0.7
+        },
         render: {
             element: document.body,
             controller: Matter.Render,
-            options: { width: W, height: H }
+            options: {
+                width: W,
+                height: H,
+                wireframes: true,
+                showVelocity: true,
+                showPosition: true
+            }
+        },
+        world: {
+            gravity: { x : 0, y : 0 }
         }
     });
 
-    engine.world.gravity.y = 0; // was .2
-    engine.world.gravity.x = 0;
+    // other stuff
 
-
-    //    engine.world.bounds.min.x = 40;
-    //    engine.world.bounds.min.y = 0;
-    //
-    //    engine.world.bounds.max.x = 200-40;
-    //    engine.world.bounds.max.y = 200-40;
-
-
-    engine.timing.timeScale *= 0.7;
-
-    let renderOptions = engine.render.options;
-    renderOptions.wireframes = true;
-    // renderOptions.hasBounds = false;
-    // renderOptions.showDebug = false;
-    // renderOptions.showBroadphase = false;
-    // renderOptions.showBounds = false;
-    renderOptions.showVelocity = true;
-    //renderOptions.showCollisions = false;
-    //renderOptions.showAxes = true;
-    renderOptions.showPositions = true;
-    //renderOptions.showAngleIndicator = true;
-    //renderOptions.showIds = true;
-    //renderOptions.showShadows = false;
-
-    //var vertices = [{x: 0, y: 150}, {x: 100, y:150}, {x: 50, y: 0}];
-    //var vertices = [{x: 0, y: 0}, {x: 100, y:0}, {x: 50, y: -150}];
-
-    let vertices = [{ x: 0, y: 0 }, { x: 10, y: 0 }, { x: 5, y: -20 }];
-
-    let ship = Bodies.fromVertices(100, 500,
-        Matter.Vertices.create(vertices), { restitution: 0.5 }); // , friction: 0.01
+    const ship = new Ship(100,500);
 
     const bodies = [
-        ship,
+        ship.body,
         // world bounds:
-        Bodies.rectangle(W / 2, H, W, 20, { isStatic: true }),
-        Bodies.rectangle(0, H / 2, 20, H, { isStatic: true }),
-        Bodies.rectangle(W, H / 2, 20, H, { isStatic: true }),
-        Bodies.rectangle(W / 2, 0, W, 20, { isStatic: true })
+        Bounds.make(W, H, Bounds.Position.UP),
+        Bounds.make(W, H, Bounds.Position.DOWN),
+        Bounds.make(W, H, Bounds.Position.LEFT),
+        Bounds.make(W, H, Bounds.Position.RIGHT)
     ];
 
     const ground: { x: number, y: number }[] = [];
@@ -125,69 +193,64 @@ function main() {
     // run the engine
     Engine.run(engine);
 
-    let keys = {};
+    const keys = {};
     window.onkeydown = e  => keys[e.keyCode] = true;
     window.onkeyup = e => keys[e.keyCode] = false;
 
-    const forceMagnitude = 0.0005 * ship.mass;
-    var defaultAirFriction = ship.airFriction;
+    // assumes 'x' between [0..H] and converts to [0..2]
+    const modifier3 = (x: number) => [-1, 0, 1][Math.floor(x / (H / 3))];
 
     Events.on(engine, 'beforeUpdate', function() {
+
+        //
+        // Gravities
+        //
         
         const bs = Matter.Composite.allBodies(engine.world);
-        // x must be between 0 and H
-        const modifier = (x : number) => [-1,0,1][Math.floor(x/(H/3))];
         
-        for(let i = 0; i<bs.length;++i){
-            const g = modifier(bs[i].position.y) * .2;
+        for(let i = 0; i<bs.length; ++i){
+            const g = modifier3(bs[i].position.y) * .2;
             bs[i].force.y += bs[i].mass * g * 0.001;            
         }
 
+        //
+        // Ship controls
+        //
+
         if (keys[VK.W] || keys[VK.UP]) {
-            var vector = Vector.rotate({ x: 0, y: -forceMagnitude }, ship.angle);
-            Body.applyForce(ship, ship.position, vector);
+            ship.thrust();
         }
 
-        function aux(isLeft: boolean) {
-            const m = isLeft ? 1 : -1;
-            //var forceMagnitude = 25 * body.mass;
-            const forceMagnitude = 0.001 * ship.mass;
-            const mx = 5; // was 50, 200
-            const my = 20;
-
-            Body.applyForce(ship, Matter.Vector.rotate({ x: m * (-mx), y: m * my }, ship.angle),
-                Matter.Vector.rotate({ x: 0, y: forceMagnitude }, ship.angle)
-                );
-
-            Body.applyForce(ship, Matter.Vector.rotate({ x: m * mx, y: m * (-my) }, ship.angle),
-                Matter.Vector.rotate({ x: 0, y: -forceMagnitude }, ship.angle)
-                );
-        };
-
+        // these controls are way too hard...
         if (keys[VK.E]) {
-            aux(false);
+            // aux(false);
+            ship.rotate(false);
         }
         if (keys[VK.Q]) {
-            aux(true);
+            // aux(true);
+            ship.rotate(true);
         }
 
+        // break / slow down
         if (keys[VK.S] || keys[VK.DOWN]) {
-            const factor = 0.99;
-            Body.setVelocity(ship, { x: ship.velocity.x * factor, y: ship.velocity.y * factor });
-            Body.setAngularVelocity(ship, ship.angularVelocity * factor);
+            ship.brake();
         }
 
+        // standard rotate right/left
         if (keys[VK.LEFT] || keys[VK.A]) {
-            Matter.Body.rotate(ship, -Math.PI / 32);
-            Body.setAngularVelocity(ship, 0);
+            ship.turnLeft();
         }
         if (keys[VK.RIGHT] || keys[VK.D]) {
-            Matter.Body.rotate(ship, Math.PI / 32);
-            Body.setAngularVelocity(ship, 0);
+            ship.turnRight();
         }
 
-        out.innerHTML = '[ '+ship.position.x.toFixed(1) + ', ' + ship.position.y.toFixed(1) + ' ]<br/>'+
-            '[ ' + ship.velocity.x.toFixed(1) + ', ' + ship.velocity.y.toFixed(1) + ' ] ' + ship.angularVelocity;
+        //
+        // HUD
+        //
+
+        const b = ship.body;
+        hud.innerHTML = '[ '+b.position.x.toFixed(1) + ', ' + b.position.y.toFixed(1) + ' ]<br/>'+
+            '[ ' + b.velocity.x.toFixed(1) + ', ' + b.velocity.y.toFixed(1) + ' ] ' + b.angularVelocity;
     });
 
-}
+};
